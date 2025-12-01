d4c42dec4641dbe9979d057150aca7d7f40f8235
Marverlises <1963835770@qq.com>
2025-12-01 20:15:43 +0800

    Format log

--- exps/exp2.sh
+++ exps/exp2.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 cd ..
-export CUDA_VISIBLE_DEVICES=1
+#export CUDA_VISIBLE_DEVICES=1
+source /share/project/baiyu/interpreter/.virtualenvs/dcase_env/bin/activate
 
 python -m d25_t6.train \
     --enable_intra_modal_alignment \
     --enable_alignment_loss \
     --alignment_loss_weight=0.1 \
     --batch_size=32 \
-    --exp_name=alignment_clotho_low_weight \
-    --clotho
+    --exp_name=alignment_audiocap_low_weight-reproduce \
+    --audiocaps \
+    --load_ckpt_path=/share/project/baiyu/project/dcase2025_task6_baseline/checkpoints/alignment_audiocap_low_weight-reproduce/last.ckpt \
+    --max_epochs=4
